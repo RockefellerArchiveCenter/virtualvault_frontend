97050a33d3134a8694de2bf267c49201f36b1152
Hillel Arnold <hillel.arnold@gmail.com>
2026-08-04 18:10:39 -0400

    make clients async

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
 
-const connector = new ElasticsearchAPIConnector({
-  host: process.env.ELASTICSEARCH_HOST,
-  index: process.env.ELASTICSEARCH_INDEX,
-});
-
 export async function POST(req) {
+  const connector = new ElasticsearchAPIConnector({
+    host: process.env.ELASTICSEARCH_HOST,
+    index: process.env.ELASTICSEARCH_INDEX,
+  });
+
   const { requestState, queryConfig } = await req.json();
 
   const response = await connector.onSearch(
