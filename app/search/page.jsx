79e863f202ac1ae42459be476906a816a351782a
Hillel Arnold <hillel.arnold@gmail.com>
2026-08-04 18:10:54 -0400

    use Suspense

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,5 +1,6 @@
+import { Suspense } from "react";
 import SearchPage from "../components/SearchPage";
 
 export default function Page() {
-  return <SearchPage />;
+  return <Suspense><SearchPage /></Suspense>;
 }
