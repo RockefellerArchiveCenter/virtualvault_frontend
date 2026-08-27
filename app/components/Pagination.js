62251eb06fcac8add2ad685924a2b5dbb1517bc5
Hillel Arnold <hillel.arnold@gmail.com>
2026-08-27 01:34:43 -0400

    update paginginfo

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -4,9 +4,26 @@ import {
 } from "@elastic/react-search-ui";
 
 
+const PagingInfoView = ({className,
+  end,
+  searchTerm,
+  start,
+  totalResults}) => {
+    return (
+        <div className="pagination__info">
+        Showing{" "}
+        <strong>
+            {start} - {end}
+        </strong>{" "}
+        out of <strong>{totalResults}</strong>
+        </div>
+    );
+}
+
+
 const Pagination = () => (
     <div className="pagination__wrapper">
-        <PagingInfo />
+        <PagingInfo view={PagingInfoView}/>
         <Paging 
             className="pagination__list"
             prevIcon={<span aria-hidden="true" className="material-icon">keyboard_arrow_left</span>}
