dde6186ddc068ce8a287cb59328936a91c9e9f45
Hillel Arnold <hillel.arnold@gmail.com>
2026-08-26 20:22:20 -0400

    add page titles in head

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,11 @@
 import { Suspense } from "react";
 import HomePage from "./components/HomePage";
 
+export const metadata = {
+  title: "Virtual Vault",
+  description: "Access digitized content"
+}
+
 export default function Page() {
   return <Suspense><HomePage /></Suspense>;
 }
