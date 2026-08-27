3147bc654741f29fa4f3ceeb183acbb080d363fb
Hillel Arnold <hillel.arnold@gmail.com>
2026-08-26 20:10:28 -0400

    improve responsiveness of item page layout

--- app/components/ItemDetail.js
+++ app/components/ItemDetail.js
@@ -41,7 +41,7 @@ const Viewer = ({ category, url }) => {
 }
 
 const DownloadButton = ({ url }) => (
-  <a className="btn btn--md btn--orange ml-10" href={url}>Download</a>
+  <a className="btn btn--md btn--orange mb-10" href={url}>Download</a>
 )
 
 const noteTitle = (noteType) => {
@@ -69,7 +69,7 @@ const Metadata = ({notes}) => {
 }
 
 const DimesLink = ({dimesId}) => (
-  <a className="btn btn--md btn--blue" href={`${process.env.DIMES_HOST}/objects/${dimesId}`}>
+  <a className="btn btn--md btn--blue mr-10 mb-10" href={`${process.env.DIMES_HOST}/objects/${dimesId}`}>
     View in DIMES
   </a>
 )
@@ -80,7 +80,7 @@ export default function ItemDetail({ data, assetUrl }) {
 
   return (
     <div className="grid">
-      <div className="item-page__wrapper">
+      <div className="item-page__wrapper grid">
         <div className="mb-20 item-page__header">
           <h1 className="mb-10">{data.title}</h1>
           <Note notes={scopeContent} />
