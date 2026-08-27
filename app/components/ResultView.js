25f4dc6ea73cc17f9e3b831eef54e440f2149c92
Hillel Arnold <hillel.arnold@gmail.com>
2026-08-27 12:54:20 -0400

    make card links work

--- app/components/ResultView.js
+++ app/components/ResultView.js
@@ -3,9 +3,7 @@ const ResultView = ({ result }) => {
   return (
       <li className="card">
         <div className="card__body">
-          <h2 className="card__title">
             <a className="card__title" href={result.url.raw}>{result.title.raw}</a>
-          </h2>
           <p className="card__body-text">
             <span className="badge badge--blue ml-10">{result.category.raw}</span>
           </p>
